0f8557f0fd660caa8d4ce4d57b2f541be575ec57
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-04-15 15:52:39 +0900

    Default.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var a = "1 + 2";
+var b = "a + 3";
+var c = {
+    apple: a,
+    banana: b
+};
+var d = c.apple + "4";
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let a: string = "1 + 2"
+let b: string = "a + 3"
+let c = {
+    apple: a,
+    banana: b
+}
+let d: string = c.apple + "4"
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
