f9ea5ca02d82148ee5343000af239bbc71717a0b
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-04-17 20:01:59 +0900

    Add, kurage.

---
+++ src/kurage.js
@@ -0,0 +1,12 @@
+var a = "~";
+var x = Math.random() < .5;
+if (x) {
+    var b = '＜コ：彡';
+    console.info(a);
+    console.info(b);
+}
+else {
+    var c = '~';
+    console.info(c);
+}
+console.info(a);
---
+++ src/kurage.ts
@@ -0,0 +1,15 @@
+let a: string = "~"
+
+let x = Math.random() < .5
+
+if(x){
+    type a = '＜コ：彡'
+    let b: string = '＜コ：彡'
+    console.info(a)
+    console.info(b)
+}else{
+    let c:string = '~'
+    console.info(c)
+}
+
+console.info(a)
