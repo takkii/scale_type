866275e987d0517d11b081dc5830f760f6610d8b
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-04-18 19:11:40 +0900

    Update, くらげの増殖。

--- src/kurage.js
+++ src/kurage.js
@@ -1,9 +1,14 @@
 var a = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-var x = Math.random() < .5;
-if (x) {
-    var b = '＜コ：彡';
+var x = 1 + Math.floor(Math.random() * 50);
+if (x < 25) {
+    var b = ' ＜コ：彡 ＜コ：彡 ＜コ：彡 ';
     console.info(a);
     console.info(b);
+}
+else if (26 < x || x < 45) {
+    console.info(a);
+    var d = ' ＜コ：彡   ＜コ：彡  ＜コ：彡  ＜コ：彡  ＜コ：彡  ＜コ：彡  ';
+    console.info(d);
 }
 else {
     var c = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
--- src/kurage.ts
+++ src/kurage.ts
@@ -1,12 +1,16 @@
 let a: string = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
-let x = Math.random() < .5
+var x = 1 + Math.floor(Math.random() * 50);
 
-if(x){
+if(x < 25){
     type a = '＜コ：彡'
-    let b: string = '＜コ：彡'
+    let b: string = ' ＜コ：彡 ＜コ：彡 ＜コ：彡 '
     console.info(a)
     console.info(b)
+}else if (26 < x || x < 45 ){
+    console.info(a)
+    let d: string = ' ＜コ：彡   ＜コ：彡  ＜コ：彡  ＜コ：彡  ＜コ：彡  ＜コ：彡  '
+    console.info(d)
 }else{
     let c:string = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     console.info(c)
