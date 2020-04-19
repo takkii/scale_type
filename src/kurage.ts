59e66b512238b45a392c6b4290078d8af8a42d89
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-04-19 19:03:36 +0900

    Update, value ctrl.

--- src/kurage.ts
+++ src/kurage.ts
@@ -1,4 +1,4 @@
-var x = 1 + Math.floor(Math.random() * 50);
+var x = 1 + Math.floor(Math.random() * 60);
 
 if(x < 20){
     let a: string = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -6,7 +6,7 @@ if(x < 20){
     console.info(a)
     console.info(b)
     console.info(a)
-} else if (21 < x || x < 41) {
+} else if (21 < x || x < 40) {
     let a: string = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     let d: string = ' ＜コ：彡   ＜コ：彡  ＜コ：彡  ＜コ：彡  ＜コ：彡  ＜コ：彡  '
     console.info(a)
